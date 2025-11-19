8705ffefc0248aa6636bc8e6b4e4fa3aecd2efe4
juho <you@example.com>
2025-11-20 01:56:21 +0200

    Revert "sql connector fix"
    This reverts commit c43eb3e91cfb0c1430e9f2fb468ffd2ee7e35d6d.

--- pages/weather.py
+++ pages/weather.py
@@ -1,8 +1,11 @@
 import streamlit as st
+import pandas as pd
 import os
 
 conn = st.connection('mysql', type='sql')
-df = conn.query('SELECT * FROM weather_data ORDER BY timestamp DESC LIMIT 50', ttl=0)
+
+df = pd.read_sql('SELECT * FROM weather_data ORDER BY timestamp DESC LIMIT 50', conn)
+conn.close()
 
 st.title('Säädata Helsingistä')
 st.dataframe(df)
