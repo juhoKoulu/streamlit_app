c43eb3e91cfb0c1430e9f2fb468ffd2ee7e35d6d
juho <you@example.com>
2025-11-20 01:52:45 +0200

    sql connector fix

--- pages/weather.py
+++ pages/weather.py
@@ -1,11 +1,8 @@
 import streamlit as st
-import pandas as pd
 import os
 
 conn = st.connection('mysql', type='sql')
-
-df = pd.read_sql('SELECT * FROM weather_data ORDER BY timestamp DESC LIMIT 50', conn)
-conn.close()
+df = conn.query('SELECT * FROM weather_data ORDER BY timestamp DESC LIMIT 50', ttl=0)
 
 st.title('Säädata Helsingistä')
 st.dataframe(df)
