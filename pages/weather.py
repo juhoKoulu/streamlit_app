16b6742a50f6c841a464a60f28a61fb074382a09
juho <you@example.com>
2025-11-20 01:47:43 +0200

    use streamlit mysql

--- pages/weather.py
+++ pages/weather.py
@@ -1,12 +1,9 @@
 import streamlit as st
-import mysql.connector
 import pandas as pd
 import os
 
-mysql_user = os.getenv("MYSQL_USER");
-mysql_password = os.getenv("MYSQL_PASSWORD");
+conn = st.connection('mysql', type='sql')
 
-conn = mysql.connector.connect(host='localhost', user=mysql_user, password=mysql_password, database='weather_db')
 df = pd.read_sql('SELECT * FROM weather_data ORDER BY timestamp DESC LIMIT 50', conn)
 conn.close()
 
