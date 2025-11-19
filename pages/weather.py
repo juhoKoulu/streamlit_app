7aaa9c7c64bc0692e4f420ca8f427f657cdafd59
juho <you@example.com>
2025-11-20 01:56:34 +0200

    Revert "use streamlit mysql"
    This reverts commit 16b6742a50f6c841a464a60f28a61fb074382a09.

--- pages/weather.py
+++ pages/weather.py
@@ -1,9 +1,12 @@
 import streamlit as st
+import mysql.connector
 import pandas as pd
 import os
 
-conn = st.connection('mysql', type='sql')
+mysql_user = os.getenv("MYSQL_USER");
+mysql_password = os.getenv("MYSQL_PASSWORD");
 
+conn = mysql.connector.connect(host='localhost', user=mysql_user, password=mysql_password, database='weather_db')
 df = pd.read_sql('SELECT * FROM weather_data ORDER BY timestamp DESC LIMIT 50', conn)
 conn.close()
 
